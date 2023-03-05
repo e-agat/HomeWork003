c83864da50cac9a34e1c7e25815df6637f9ca65f
Evgeniya <kapatoy@yandex.ru>
2023-03-05 17:13:02 +0300

    HW003_05.03

---
+++ HW003_05.03/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.Clear();
+
+void FillArray(int[] array, int min, int max)
+{
+    int arrayLength = array.Length;
+    for (int i = 0; i < arrayLength; i++)
+    {
+
+        array[i] = new Random().Next(min, max);
+    }
+
+}
+void PrintArray(int[] array,int min, int max)
+{
+    Console.WriteLine("");
+
+}
+int[] array = new int[8];
+int a = 1;
+int b = 45;
+FillArray(array,a,b);
+PrintArray(array,a,b);
+Console.WriteLine($"[{string.Join(", ", array)}]");
